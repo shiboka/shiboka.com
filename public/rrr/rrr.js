94362e404dbc5f9dcc6dab750054c90347d9e8fd
Shiboka <d.aubie@outlook.com>
2024-06-04 00:52:37 -0300

    Update variable names in rrr.js

--- public/rrr/rrr.js
+++ public/rrr/rrr.js
@@ -7,49 +7,49 @@ class ImageRenderer {
   constructor() {
     this.interval = null;
     this.delay = 10;
-    this.pxl_max_size = 80;
-    this.pxl_min_size = 4;
-    this.pxl_size = this.pxl_max_siz
-    this.pxl_amp = 20;
-    this.pxl_amount = 1;
-    this.render_step = 0;
-    this.img_ref = document.getElementById('img-ref');
-    this.img_ref_ctx = this.img_ref.getContext('2d');
+    this.pxlMaxSize = 80;
+    this.pxlMinSize = 4;
+    this.pxlSize = this.pxlMaxSize;
+    this.pxlAmp = 20;
+    this.pxlAmount = 1;
+    this.renderStep = 0;
+    this.imgRef = document.getElementById('img-ref');
+    this.imgRefCtx = this.imgRef.getContext('2d');
     this.cnv = document.getElementById('cnv');
-    this.cnv_ctx = this.cnv.getContext('2d');
+    this.cnvCtx = this.cnv.getContext('2d');
   }
   
   renderPixels() {
     try {
       // makes the size decrease at a nice rate
-      if (!(this.render_step % (Math.floor(this.pxl_max_size / this.pxl_size) * this.pxl_amp)) && this.pxl_size > this.pxl_min_size)
-        this.pxl_size--;
+      if (!(this.renderStep % (Math.floor(this.pxlMaxSize / this.pxlSize) * this.pxlAmp)) && this.pxlSize > this.pxlMinSize)
+        this.pxlSize--;
       
       // increase amount of rects per 200 iterations
-      if (!(this.render_step % MAX_STEP))
-        this.pxl_amount++;
+      if (!(this.renderStep % MAX_STEP))
+        this.pxlAmount++;
       
       // this is as high as we need to count to
-      if (this.render_step == this.pxl_max_size * this.pxl_pxl_amp)
-        this.render_step = 0;
+      if (this.renderStep == this.pxlMaxSize * this.pxlAmp)
+        this.renderStep = 0;
       
       // finally, draw the rects onto the canvas.
-      for (let i = 0; i < this.pxl_amount; i++) {
+      for (let i = 0; i < this.pxlAmount; i++) {
         let x = Math.floor(Math.random() * this.cnv.width);
         let y = Math.floor(Math.random() * this.cnv.height);
         let pxl;
 
         try {
-          pxl = this.img_ref_ctx.getImageData(x, y, 1, 1).data;
+          pxl = this.imgRefCtx.getImageData(x, y, 1, 1).data;
         } catch (e) {
           console.error(`Error getting image data: ${e}`);
         }
 
-        this.cnv_ctx.fillStyle = `rgb(${pxl[0]}, ${pxl[1]}, ${pxl[2]})`;
-        this.cnv_ctx.fillRect(x - this.pxl_size / 2, y - this.pxl_size / 2, this.pxl_size, this.pxl_size);
+        this.cnvCtx.fillStyle = `rgb(${pxl[0]}, ${pxl[1]}, ${pxl[2]})`;
+        this.cnvCtx.fillRect(x - this.pxlSize / 2, y - this.pxlSize / 2, this.pxlSize, this.pxlSize);
       }
       
-      this.render_step++;
+      this.renderStep++;
     } catch (e) {
       console.error(`Error rendering pixels: ${e}`);
     }
@@ -74,27 +74,27 @@ class ImageRenderer {
           this.width = maxWidth;
         } else {
           this.width = windowWidth - WINDOW_PADDING;
-          this.pxl_max_size = 60;
-          this.pxl_min_size = 2;
+          this.pxlMaxSize = 60;
+          this.pxlMinSize = 2;
         }
       
         this.height = this.width * aspectRatio;
       
-        this.img_ref.width = this.width;
-        this.img_ref.height = this.height;
+        this.imgRef.width = this.width;
+        this.imgRef.height = this.height;
         this.cnv.width = this.width;
         this.cnv.height = this.height;
       
-        this.img_ref_ctx.drawImage(this.img, 0, 0, this.width, this.height);
+        this.imgRefCtx.drawImage(this.img, 0, 0, this.width, this.height);
       
         // reset values for the render loop
-        this.render_step = 0;
-        this.pxl_amount = 1;
-        this.pxl_size = this.pxl_max_size;
+        this.renderStep = 0;
+        this.pxlAmount = 1;
+        this.pxlSize = this.pxlMaxSize;
       
         // clear canvas and start render loop
-        this.cnv_ctx.fillStyle = 'black';
-        this.cnv_ctx.fillRect(0, 0, this.width, this.height);
+        this.cnvCtx.fillStyle = 'black';
+        this.cnvCtx.fillRect(0, 0, this.width, this.height);
         this.interval = window.setInterval(() => this.renderPixels(), this.delay);
       });
     } catch (e) {
