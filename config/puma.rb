b230a41264b8b3ba7fde92af539251be4648d1ed
shiboka <d.aubie@outlook.com>
2025-01-02 18:51:49 -0400

    some cleanup

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 environment "development"
 port 3000
-threads 1, 1
+threads 0, 1
 workers 0
 preload_app!
--- config/puma_production.rb
+++ config/puma_production.rb
@@ -1,5 +1,5 @@
 environment "production"
 port 80
-threads 1, 1
+threads 0, 1
 workers 0
 preload_app!
