b57ea718499f12f452830b80d30a2c50eae68f55
Shiboka <d.aubie@outlook.com>
2025-01-21 09:40:53 -0400

    nothing

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className='home-content-wrapper'>
+      <div className='home-content'>
+        <div className='home-content-header'>
+        </div>
+        <div className='home-content-list'>
+          <div className='home-content-list-item'></div>
+          <div className='home-content-list-item'></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './Home.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path='home' element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
